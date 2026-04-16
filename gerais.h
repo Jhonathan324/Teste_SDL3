aeeefcf364745dcac6b7dacd827442d96433360e
Jhonathan324 <158005754+Jhonathan324@users.noreply.github.com>
2026-04-16 19:02:06 -0300

    Add files via upload

---
+++ gerais.h
@@ -0,0 +1,32 @@
+#ifndef GERAIS_H_INCLUDED
+#define GERAIS_H_INCLUDED
+#include <SDL3/SDL.h>
+
+enum ESTADO_JOGO{
+    CENA_MENU,
+    CENA_JOGO,
+    CENA_PAUSE,
+    CENA_CONF,
+    CENA_SAIR,
+    CENA_CREDITOS,
+    CENA_MENU_RESOLUCAO
+};
+
+
+typedef struct Player{
+    int vida;
+    int coracoes;
+    int velocidade;
+    SDL_Rect retangulo;
+} Player;
+
+typedef struct Botao{
+    int estado;
+    char texto[30];
+    int cor[3];
+    SDL_Rect retangulo;
+} Botao;
+
+
+
+#endif // GERAIS_H_INCLUDED
--- main.c
+++ main.c
@@ -1,78 +1,80 @@
 #include <SDL3/SDL.h>
 #include <stdbool.h>
+#include "gerais.h"
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("Jogo SDL3", 800, 600, 0);
+    SDL_Window *window = SDL_CreateWindow("Teste", 800, 600, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
+    SDL_Event evento;
+
     bool rodando = true;
-    SDL_Event event;
 
-    // Player
-    float px = 100, py = 100;
-    float speed = 5;
 
-    // Inimigo
-    float ex = 400, ey = 300;
-    float espeed = 3;
+    int cena = CENA_MENU;
+
+    Botao botao_iniciar = {0, "Iniciar Jogo", (int){40,40,40}, (SDL_Rect){300,300,200,60}};
+    mouse_x, mouse_y;
+    bool mouse = SDL_MouseButtonEvent(&mouse_x, &mouse_y);
+
+
+
+
+    Player jogador = {100, 3, 5, (SDL_Rect){100,100,50,50}};
+    int velocidade_jogador_x = 0;
+    int velocidade_jogador_y = 0;
+
 
     while (rodando) {
 
-        // Eventos
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+        while (SDL_PollEvent(&evento)) {
+            if (evento.type == SDL_EVENT_QUIT) {
                 rodando = false;
             }
         }
 
-        // Teclado
-        const bool *keys = SDL_GetKeyboardState(NULL);
-
-        if (keys[SDL_SCANCODE_W]) py -= speed;
-        if (keys[SDL_SCANCODE_S]) py += speed;
-        if (keys[SDL_SCANCODE_A]) px -= speed;
-        if (keys[SDL_SCANCODE_D]) px += speed;
-
-        // IA simples (inimigo segue o player)
-        if (px > ex) ex += espeed;
-        if (px < ex) ex -= espeed;
-        if (py > ey) ey += espeed;
-        if (py < ey) ey -= espeed;
-
-        // Colisão (AABB simples)
-        if (px < ex + 50 &&
-            px + 50 > ex &&
-            py < ey + 50 &&
-            py + 50 > ey) {
-
-            SDL_ShowSimpleMessageBox(
-                SDL_MESSAGEBOX_ERROR,
-                "Game Over",
-                "Você perdeu!",
-                window
-            );
-
-            rodando = false;
-        }
-
-        // Render
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Player (verde)
-        SDL_FRect player = { px, py, 50, 50 };
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        SDL_RenderFillRect(renderer, &player);
 
-        // Inimigo (vermelho)
-        SDL_FRect enemy = { ex, ey, 50, 50 };
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &enemy);
+        switch(cena){
+        case(CENA_MENU):
+            bool mouse = SDL_MouseButtonEvent(&mouse_x, &mouse_y);
+            SDL_SetRenderDrawColor(renderer, botao_iniciar.cor[0], botao_iniciar.cor[1], botao_iniciar.cor[2], 255);
+            SDL_RenderClear(renderer);
+            SDL_RenderFillRect(renderer,botao_iniciar.retangulo);
+
+
+
+
+
+
+            break;
+
+
+
+
+
+
+
+        case(CENA_JOGO:
+            const bool *teclado = SDL_GetKeyboardState(NULL);
+
+            if (teclado[SDL_SCANCODE_W]) velocidade_jogador_x -= jogador.velocidade;
+            if (teclado[SDL_SCANCODE_S]) velocidade_jogador_x += jogador.velocidade;
+            if (teclado[SDL_SCANCODE_A]) velocidade_jogador_x -= jogador.velocidade;
+            if (teclado[SDL_SCANCODE_D]) velocidade_jogador_x += jogador.velocidade;
+
+            SDL_FRect enemy = { ex, ey, 50, 50 };
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_RenderFillRect(renderer, &enemy);
+            break;
+        }
+        // Limpar a Tela
 
         SDL_RenderPresent(renderer);
-
         SDL_Delay(16); // ~60 FPS
     }
 
