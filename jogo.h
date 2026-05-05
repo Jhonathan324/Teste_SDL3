4deed678eb49e2336a8cae8ac71ecee24a0ca43d
Jhonathan324 <158005754+Jhonathan324@users.noreply.github.com>
2026-05-04 21:49:31 -0300

    Add files via upload

--- jogo.h
+++ jogo.h
@@ -5,13 +5,24 @@
 #define TamanhosMapaX 512
 #define TamanhosMapaY 128
 
+typedef struct PlayerInJogo
+{
+    int vida;
+    int coracoes;
+    float velocidade;
+    SDL_FRect retangulo;
+}PlayerInJogo;
+
 typedef struct VariveisJogo
 {
     SDL_Color cor_fundo;
     int velocidade_jogador_x;
     int velocidade_jogador_y;
     uint8_t mapa[TamanhosMapaX][TamanhosMapaY];
+    PlayerinJogo jogodor;
 } VariveisJogo;
+
+
 
 void InitJogo(VariveisGerais *geral, VariveisJogo *jogo, TAMANHOS tamanhos);
 void CenaJogoLoop(VariveisGerais *geral, VariveisJogo *jogo);
